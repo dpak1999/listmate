a632ba851ca26a6a815da6045b27dd0e4de6e8c9
Deepak <deepakkumardash313@gmail.com>
2025-01-09 17:22:00 +0530

    Add sign in amd sign up flow

--- client/app/(auth)/index.tsx
+++ client/app/(auth)/index.tsx
@@ -1,7 +1,11 @@
 import { ThemedText } from "@/components/ThemedText";
-import { useSignIn } from "@clerk/clerk-expo";
-import { Link, useRouter } from "expo-router";
-import { useState } from "react";
+import { BodyScrollView } from "@/components/ui/body-scroll-view";
+import { Button } from "@/components/ui/button";
+import { TextInput } from "@/components/ui/text-input";
+import { isClerkAPIResponseError, useSignIn } from "@clerk/clerk-expo";
+import { ClerkAPIError } from "@clerk/types";
+import { useRouter } from "expo-router";
+import { useCallback, useState } from "react";
 import { View } from "react-native";
 
 export default function SignInScreen() {
@@ -11,14 +15,76 @@ export default function SignInScreen() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [isSigningIn, setIsSigningIn] = useState(false);
+  const [errors, setErrors] = useState<ClerkAPIError[]>([]);
 
-  if (!isLoaded) {
-    return null;
-  }
+  const handleSignIn = useCallback(async () => {
+    if (!isLoaded) return;
+
+    setIsSigningIn(true);
+    setErrors([]);
+    try {
+      const signInAttempt = await signIn.create({
+        identifier: email,
+        password,
+      });
+
+      if (signInAttempt.status === "complete") {
+        await setActive({ session: signInAttempt.createdSessionId });
+        router.replace("/(index)");
+      }
+    } catch (err) {
+      if (isClerkAPIResponseError(err)) setErrors(err.errors);
+    } finally {
+      setIsSigningIn(false);
+    }
+  }, [email, password, isLoaded]);
+
   return (
-    <View>
-      <ThemedText type="title">Sign in</ThemedText>
-      <Link href={"/sign-up"}>Go to sign up</Link>
-    </View>
+    <BodyScrollView contentContainerStyle={{ padding: 16 }}>
+      <TextInput
+        label="Email"
+        placeholder="Enter email"
+        autoCapitalize="none"
+        keyboardType="email-address"
+        onChangeText={setEmail}
+        value={email}
+      />
+
+      <TextInput
+        label="Password"
+        placeholder="Enter password"
+        secureTextEntry={true}
+        onChangeText={(p) => setPassword(p)}
+        value={password}
+      />
+
+      <Button
+        onPress={handleSignIn}
+        loading={isSigningIn}
+        disabled={!email || !password || isSigningIn}
+      >
+        Sign in
+      </Button>
+
+      {errors.map((err) => (
+        <ThemedText key={err.longMessage} style={{ color: "red" }}>
+          {err.longMessage}
+        </ThemedText>
+      ))}
+
+      <View style={{ marginTop: 16, alignItems: "center" }}>
+        <ThemedText>Don't have an account?</ThemedText>
+        <Button variant="ghost" onPress={() => router.replace("/sign-up")}>
+          Sign up
+        </Button>
+      </View>
+
+      <View style={{ marginTop: 16, alignItems: "center" }}>
+        <ThemedText>Forgot password?</ThemedText>
+        <Button variant="ghost" onPress={() => router.push("/reset-password")}>
+          Reset password
+        </Button>
+      </View>
+    </BodyScrollView>
   );
 }
--- client/app/(auth)/sign-up.tsx
+++ client/app/(auth)/sign-up.tsx
@@ -1,10 +1,127 @@
 import { ThemedText } from "@/components/ThemedText";
+import { BodyScrollView } from "@/components/ui/body-scroll-view";
+import { Button } from "@/components/ui/button";
+import { TextInput } from "@/components/ui/text-input";
+import { isClerkAPIResponseError, useSignUp } from "@clerk/clerk-expo";
+import { ClerkAPIError } from "@clerk/types";
+import { useRouter } from "expo-router";
+import { useState } from "react";
 import { View } from "react-native";
 
 export default function SignUpScreen() {
+  const { signUp, setActive, isLoaded } = useSignUp();
+  const router = useRouter();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isSigningUp, setIsSigningUp] = useState(false);
+  const [errors, setErrors] = useState<ClerkAPIError[]>([]);
+  const [code, setCode] = useState("");
+  const [pendingVerification, setPendingVerification] = useState(false);
+
+  const handleSignUp = async () => {
+    if (!isLoaded) return;
+    setIsSigningUp(true);
+    setErrors([]);
+
+    try {
+      await signUp.create({ emailAddress: email, password: password });
+      await signUp.prepareEmailAddressVerification({ strategy: "email_code" });
+      setPendingVerification(true);
+    } catch (error) {
+      if (isClerkAPIResponseError(error)) setErrors(error.errors);
+    } finally {
+      setIsSigningUp(false);
+    }
+  };
+
+  const handleVerify = async () => {
+    if (!isLoaded) return;
+    setIsSigningUp(true);
+    setErrors([]);
+
+    try {
+      const signupAttempt = await signUp.attemptEmailAddressVerification({
+        code,
+      });
+
+      if (signupAttempt.status === "complete") {
+        await setActive({ session: signupAttempt.createdSessionId });
+        router.replace("/");
+      }
+    } catch (error) {
+      if (isClerkAPIResponseError(error)) setErrors(error.errors);
+    } finally {
+      setIsSigningUp(false);
+    }
+  };
+
+  if (pendingVerification) {
+    return (
+      <BodyScrollView contentContainerStyle={{ padding: 16 }}>
+        <TextInput
+          label={`Please enter verification code sent to ${email}`}
+          placeholder="Enter verification code"
+          onChangeText={(c) => setCode(c)}
+          value={code}
+        />
+
+        <Button
+          onPress={handleVerify}
+          loading={isSigningUp}
+          disabled={!code || isSigningUp}
+        >
+          Verify
+        </Button>
+
+        {errors.map((err) => (
+          <ThemedText key={err.longMessage} style={{ color: "red" }}>
+            {err.longMessage}
+          </ThemedText>
+        ))}
+      </BodyScrollView>
+    );
+  }
+
   return (
-    <View>
-      <ThemedText type="title">Sign up</ThemedText>
-    </View>
+    <BodyScrollView contentContainerStyle={{ padding: 16 }}>
+      <TextInput
+        label="Email"
+        placeholder="Enter email"
+        autoCapitalize="none"
+        keyboardType="email-address"
+        onChangeText={(email) => setEmail(email)}
+        value={email}
+      />
+
+      <TextInput
+        label="Password"
+        placeholder="Enter password"
+        secureTextEntry={true}
+        onChangeText={(password) => setPassword(password)}
+        value={password}
+      />
+
+      <Button
+        onPress={handleSignUp}
+        loading={isSigningUp}
+        disabled={!email || !password || isSigningUp}
+      >
+        Continue
+      </Button>
+
+      {errors.map((err) => (
+        <ThemedText key={err.longMessage} style={{ color: "red" }}>
+          {err.longMessage}
+        </ThemedText>
+      ))}
+
+      <View style={{ marginTop: 16, alignItems: "center" }}>
+        <ThemedText>Already have an account?</ThemedText>
+        <Button variant="ghost" onPress={() => router.replace("/")}>
+          Sign in
+        </Button>
+      </View>
+    </BodyScrollView>
   );
 }
