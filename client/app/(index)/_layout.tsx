cfcf293d2e951421f3eabaa2be82bb2e1b140005
Deepak <deepakkumardash313@gmail.com>
2025-01-10 14:38:17 +0530

    Add persitor for tinybase

--- client/app/(index)/_layout.tsx
+++ client/app/(index)/_layout.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button";
 import { ListCreationProvider } from "@/context/ListCreationContext";
 import { useUser } from "@clerk/clerk-expo";
 import { Redirect, router, Stack } from "expo-router";
+import { Provider as TinybaseProvider } from "tinybase/ui-react";
 
 export default function HomeRoutesLayout() {
   const { user } = useUser();
@@ -11,75 +12,80 @@ export default function HomeRoutesLayout() {
   }
 
   return (
-    <ListCreationProvider>
-      <Stack
-        screenOptions={{
-          ...(process.env.EXPO_OS !== "ios"
-            ? {}
-            : {
-                headerLargeTitle: true,
-                headerTransparent: true,
-                headerBlurEffect: "systemChromeMaterial",
-                headerLargeTitleShadowVisible: false,
-                headerShadowVisible: true,
-                headerLargeStyle: {
-                  backgroundColor: "transparent",
-                },
-              }),
-        }}
-      >
-        <Stack.Screen name="index" options={{ headerTitle: "Shopping List" }} />
-        <Stack.Screen
-          name="list/new/index"
-          options={{
-            presentation: "formSheet",
-            sheetGrabberVisible: true,
-            headerShown: false,
+    <TinybaseProvider>
+      <ListCreationProvider>
+        <Stack
+          screenOptions={{
+            ...(process.env.EXPO_OS !== "ios"
+              ? {}
+              : {
+                  headerLargeTitle: true,
+                  headerTransparent: true,
+                  headerBlurEffect: "systemChromeMaterial",
+                  headerLargeTitleShadowVisible: false,
+                  headerShadowVisible: true,
+                  headerLargeStyle: {
+                    backgroundColor: "transparent",
+                  },
+                }),
           }}
-        />
-        <Stack.Screen
-          name="profile"
-          options={{
-            presentation: "formSheet",
-            sheetGrabberVisible: true,
-            headerShown: false,
-            sheetAllowedDetents: [0.75, 1],
-          }}
-        />
-        <Stack.Screen
-          name="list/new/scan"
-          options={{
-            presentation: "fullScreenModal",
-            headerLargeTitle: false,
-            headerTitle: "Scan a QR code",
-            headerLeft: () => (
-              <Button variant="ghost" onPress={() => router.back()}>
-                Cancel
-              </Button>
-            ),
-          }}
-        />
-        <Stack.Screen
-          name="emoji-picker"
-          options={{
-            presentation: "formSheet",
-            headerLargeTitle: false,
-            headerTitle: "Choose an emoji",
-            sheetAllowedDetents: [0.5, 0.75, 1],
-            sheetGrabberVisible: true,
-          }}
-        />
-        <Stack.Screen
-          name="color-picker"
-          options={{
-            presentation: "formSheet",
-            headerLargeTitle: false,
-            headerTitle: "Choose a color",
-            sheetAllowedDetents: [0.5, 0.75, 1],
-            sheetGrabberVisible: true,
-          }}
-        />
-      </Stack>
-    </ListCreationProvider>
+        >
+          <Stack.Screen
+            name="index"
+            options={{ headerTitle: "Shopping List" }}
+          />
+          <Stack.Screen
+            name="list/new/index"
+            options={{
+              presentation: "formSheet",
+              sheetGrabberVisible: true,
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name="profile"
+            options={{
+              presentation: "formSheet",
+              sheetGrabberVisible: true,
+              headerShown: false,
+              sheetAllowedDetents: [0.75, 1],
+            }}
+          />
+          <Stack.Screen
+            name="list/new/scan"
+            options={{
+              presentation: "fullScreenModal",
+              headerLargeTitle: false,
+              headerTitle: "Scan a QR code",
+              headerLeft: () => (
+                <Button variant="ghost" onPress={() => router.back()}>
+                  Cancel
+                </Button>
+              ),
+            }}
+          />
+          <Stack.Screen
+            name="emoji-picker"
+            options={{
+              presentation: "formSheet",
+              headerLargeTitle: false,
+              headerTitle: "Choose an emoji",
+              sheetAllowedDetents: [0.5, 0.75, 1],
+              sheetGrabberVisible: true,
+            }}
+          />
+          <Stack.Screen
+            name="color-picker"
+            options={{
+              presentation: "formSheet",
+              headerLargeTitle: false,
+              headerTitle: "Choose a color",
+              sheetAllowedDetents: [0.5, 0.75, 1],
+              sheetGrabberVisible: true,
+            }}
+          />
+        </Stack>
+      </ListCreationProvider>
+    </TinybaseProvider>
   );
 }
---
+++ client/stores/persistence/useCreateClientPersisterAndStart.ts
@@ -0,0 +1,33 @@
+import * as UiReact from "tinybase/ui-react/with-schemas";
+import {
+  Content,
+  MergeableStore,
+  OptionalSchemas,
+} from "tinybase/with-schemas";
+import { createClientPersister } from "./createClientPersister";
+
+export const useCreateClientPersisterAndStart = <
+  Schemas extends OptionalSchemas
+>(
+  storeId: string,
+  store: MergeableStore<Schemas>,
+  initialContentJson?: string,
+  then?: () => void
+) =>
+  (UiReact as UiReact.WithSchemas<Schemas>).useCreatePersister(
+    store,
+    // @ts-ignore
+    (store: MergeableStore<Schemas>) => createClientPersister(storeId, store),
+    [storeId],
+    async (persister) => {
+      let initialContent: Content<Schemas> | undefined = undefined;
+      try {
+        initialContent = JSON.parse(initialContentJson ?? "{}");
+      } catch {}
+
+      await persister.load(initialContent);
+      await persister.startAutoSave();
+      then?.();
+    },
+    [initialContentJson]
+  );
